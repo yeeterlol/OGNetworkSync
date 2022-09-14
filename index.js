e5d63c4f1b2ca495a622df92d9699ab55fd44550
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-09-13 18:50:48 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// Load discord.js
+require('dotenv').config();
+
+const {
+    Client,
+    Intents
+  } = require('discord.js')
+  // Create Discord intentions, required in v13
+  const intents = new Intents(['GUILDS', 'GUILD_MESSAGES'])
+  // Create Discord client
+  const client = new Client({
+    intents
+  })
+  
+  let channel = process.env.DISCORD_CHANNEL
+  
+  // Load mineflayer
+  const mineflayer = require('mineflayer')
+  const bot = mineflayer.createBot({
+    host: "og-network.net",
+    port: "25565",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    version: process.env.VERSION,
+    auth: "microsoft"
+  })
+  
+  client.on('ready', () => {
+    console.log(`The discord bot logged in! Username: ${client.user.username}!`)
+    // Find the Discord channel messages will be sent to
+    channel = client.channels.cache.get(channel)
+    if (!channel) {
+      console.log(`I could not find the channel (${process.argv[3]})!\nUsage : node discord.js <discord bot token> <channel id> <host> <port> [<name>] [<password>]`)
+      process.exit(1)
+    }
+  })
+  
+  // Redirect Discord messages to in-game chat
+  client.on('messageCreate', message => {
+    // Only handle messages in specified channel
+    if (message.channel.id !== channel.id) return
+    // Ignore messages from the bot itself
+    if (message.author.id === client.user.id) return
+    bot.chat(`/gc ${message.author.username}: ${message.content}`)
+  })
+  
+  // Redirect in-game messages to Discord channel
+  bot.on('chat', (username, message) => {
+    // Ignore messages from the bot itself
+    if (username.startsWith("Guild") === false) return
+    if (message.startsWith(`[Tribute] ${proccess.env.USERNAME}`) === true) return
+    if (username === bot.username) return
+    
+    channel.send(`${username}: ${message}`)
+  })
+  
+  // Login Discord bot
+  client.login(process.env.TOKEN)
